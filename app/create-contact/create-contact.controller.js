c12c319abcc2a5acaa1f9237dda7852d07d09a50
Pruthviraj Patel <pruthviraj@lendingkart.com>
2017-04-12 05:17:17 +0530

    Edit form changes

--- app/create-contact/create-contact.controller.js
+++ app/create-contact/create-contact.controller.js
@@ -1,9 +1,6 @@
 app.controller('CreateContactController', ['$state', '$stateParams', 'ContactService', function($state, $stateParams, ContactService) {
     var vm = this;
-    vm.user = {};
-    if ($stateParams.index) {
-        vm.user = ContactService.getContact($stateParams.index);
-    }
+    vm.user = $stateParams.index?ContactService.getContact($stateParams.index):{};
     vm.addContact = function() {
         ContactService.addContact(vm.user);
         $state.go('list');
